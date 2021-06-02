45a4f250011cb202aa8111bbcce56e1eb395eff7
bolovtsov <bol.ser.vla@gmail.com>
2021-06-02 20:31:22 +0300

    Настройка

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+bot = telebot.TeleBot('1842565447:AAEdmcSeizPxImxGc1ZNAhkq5sIhUwm9vLg')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+bot.polling(none_stop=True)
