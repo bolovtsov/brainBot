05a53b60c8b987adc8f0dfa7c94453b08cb709c6
bolovtsov <bol.ser.vla@gmail.com>
2021-06-05 18:35:07 +0300

    Запуск

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import constans
 
 bot = telebot.TeleBot(constans.token)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills!, {message.from_user.first_name}')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -13,5 +13,12 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Привет!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+@bot.message_handler(commands = ['help'])
+def url(message):
+    markup = bot.InlineKeyboardMarkup()
+    btn_my_site = bot.InlineKeyboardButton(text='Наш сайт', url='https://brainskills.live')
+    markup.add(btn_my_site)
+    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
 
 bot.polling(none_stop=True)
