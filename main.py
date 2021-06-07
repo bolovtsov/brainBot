f0318d0c9f6ccd9e7b20aba17541622d5db1a2a8
bolovtsov <bol.ser.vla@gmail.com>
2021-06-07 21:31:16 +0300

    Запуск

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import telebot
 import constans
 
-from aiogram.types import ReplyKeyboardRemove,  ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-button_hi = KeyboardButton('Привет! 👋')
-
-greet_kb = ReplyKeyboardMarkup()
-greet_kb.add(button_hi)
-
 bot = telebot.TeleBot(constans.token)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
