07be0234d151eab1752adfdd318d111932d79b3d
bolovtsov <bol.ser.vla@gmail.com>
2021-06-07 21:50:10 +0300

    Запуск

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import constans
 
 bot = telebot.TeleBot(constans.token)
 
-
 @bot.message_handler(commands=['start'])
+def start_message(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row('Привет', 'Пока')
+    bot.send_message(message.chat.id, 'Приветствуем вас в сообществе Brainskills!!!', reply_markup=keyboard)
+
+"""@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills!!!, {message.from_user.first_name}')
+    bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills!!!, {message.from_user.first_name}')"""
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
