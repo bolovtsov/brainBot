d41a5695086ed782af4b7c3a1465f1f0ce918bbd
bolovtsov <bol.ser.vla@gmail.com>
2021-06-07 21:54:58 +0300

    Запуск

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def start_message(message):
     keyboard.row('Привет', 'Пока')
     bot.send_message(message.chat.id, 'Приветствуем вас в сообществе Brainskills!!!', reply_markup=keyboard)
 
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Три', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Четыре', callback_data=4))
+    markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
+    bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
+
 """@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills!!!, {message.from_user.first_name}')"""
@@ -20,12 +28,5 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
-@bot.message_handler(commands=['help'])
-def start_message(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='Три', callback_data=3))
-    markup.add(telebot.types.InlineKeyboardButton(text='Четыре', callback_data=4))
-    markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
-    bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
 
 bot.polling(none_stop=True)
