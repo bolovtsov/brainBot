249cf61f3655a2ef75b63e3fc71b76ebb3be31d6
bolovtsov <bol.ser.vla@gmail.com>
2021-06-08 21:43:39 +0300

    Запуск

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ def start_message(message):
     markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
     bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
 
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
+    answer = ''
+    if call.data == '3':
+        answer = 'Вы троечник!'
+    elif call.data == '4':
+        answer = 'Вы хорошист!'
+    elif call.data == '5':
+        answer = 'Вы отличник!'
+    bot.send_message(call.message.chat.id, answer)
+
 """@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills!!!, {message.from_user.first_name}')"""
