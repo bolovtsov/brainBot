ceef4da6eb22e82bcb596b8b4c8c88baf12204e3
bolovtsov <bol.ser.vla@gmail.com>
2021-06-10 21:51:27 +0300

    Запуск

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ def start_message(message):
 @bot.message_handler(commands=['help'])
 def start_message(message):
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='Введение в Python', callback_data=1))
+    markup.add(telebot.types.InlineKeyboardButton(text='SQL/Oracle', callback_data=1))
     markup.add(telebot.types.InlineKeyboardButton(text='Анализ данных на Python', callback_data=2))
-    markup.add(telebot.types.InlineKeyboardButton(text='SQL/Oracle', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Базовый Excel', callback_data=3))
     bot.send_message(message.chat.id, text="Какая программа вас интересует?", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
+    bot.answer_callback_query(callback_query_id=call.id, text='А ты в тренде!')
     answer = ''
     if call.data == '1':
-        answer = f'Программа состоит из 10 недель:\n' \
+        answer = f'SQL/Oracle это 10 недель:\n' \
                  f'1 - Связи в базах данных\n' \
                  f'2 - Создание таблиц\n' \
                  f'3 - DML и индексы \n' \
@@ -36,13 +36,13 @@ def query_handler(call):
                  f'9 - Курсоры, хранимые процедуры ' \
                  f'10 - Триггеры, исключения '
     elif call.data == '2':
-        answer = f'Программа состоит из 7 модулей:\n' \
+        answer = f'Анализ данных на Python это 7 модулей:\n' \
                  f'1 - Введение в Python\n2 - Библиотеки Python\n3 - ООП и ' \
                  f'паттерны \n4 - Математика для Анализа данных \n ' \
                  f'5 - Машинное обучение\n6 - Нейронные сети\n 7 - ' \
                  f'7 - Кейсы в портфолио '
     elif call.data == '3':
-        answer = f'Программа состоит из 6 недель:\n' \
+        answer = f'Базовый Excel это 6 недель:\n' \
                  f'1 - Структура документа Excel\n' \
                  f'2 - Базовое форматирование и фильтры\n' \
                  f'3 - Google таблицы и автозаполнение \n' \
@@ -57,8 +57,17 @@ def send_welcome(message):
     bot.reply_to(message, f'Приветствуем вас в сообществе Brainskills, {message.from_user.first_name}!\n'
                           f'Здесь вы можете:\n'
                           f'/help узнать как работает подписка Brainskills\n'
-                          f'/setting получать бесплатные материалы по Python, ML/DL, AI\n'
+                          f'/setting получить бесплатные материалы по Python, кейс и ML\n'
                           f'/action регистрироваться на бесплатные мероприятия')
+
+
+@bot.message_handler(commands=['setting'])
+def start_message(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Бесплатные материалы по Python', callback_data=1))
+    markup.add(telebot.types.InlineKeyboardButton(text='Бесплатные кейс', callback_data=2))
+    markup.add(telebot.types.InlineKeyboardButton(text='Бесплатные материалы по Python', callback_data=3))
+    bot.send_message(message.chat.id, text="Что по плечу?", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
