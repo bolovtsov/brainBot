2afd9cd98bd2dae0bfb97bd5b3bd7efc1a69cab4
bolovtsov <bol.ser.vla@gmail.com>
2021-06-10 21:43:28 +0300

    Запуск

--- main.py
+++ main.py
@@ -18,17 +18,39 @@ def start_message(message):
     markup.add(telebot.types.InlineKeyboardButton(text='SQL/Oracle', callback_data=3))
     bot.send_message(message.chat.id, text="Какая программа вас интересует?", reply_markup=markup)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
     answer = ''
-    if call.data == '3':
-        answer = 'Вы троечник!'
-    elif call.data == '4':
-        answer = 'Вы хорошист!'
-    elif call.data == '5':
-        answer = 'Вы отличник!'
+    if call.data == '1':
+        answer = f'Программа состоит из 10 недель:\n' \
+                 f'1 - Связи в базах данных\n' \
+                 f'2 - Создание таблиц\n' \
+                 f'3 - DML и индексы \n' \
+                 f'4 - Запросы к нескольким таблицам\n ' \
+                 f'5 - Условия для агрегатных функций\n ' \
+                 f'6 - Промышленный запрос\n ' \
+                 f'7 - Транзакции, представления ' \
+                 f'8 - Операторы, циклы ' \
+                 f'9 - Курсоры, хранимые процедуры ' \
+                 f'10 - Триггеры, исключения '
+    elif call.data == '2':
+        answer = f'Программа состоит из 7 модулей:\n' \
+                 f'1 - Введение в Python\n2 - Библиотеки Python\n3 - ООП и ' \
+                 f'паттерны \n4 - Математика для Анализа данных \n ' \
+                 f'5 - Машинное обучение\n6 - Нейронные сети\n 7 - ' \
+                 f'7 - Кейсы в портфолио '
+    elif call.data == '3':
+        answer = f'Программа состоит из 6 недель:\n' \
+                 f'1 - Структура документа Excel\n' \
+                 f'2 - Базовое форматирование и фильтры\n' \
+                 f'3 - Google таблицы и автозаполнение \n' \
+                 f'4 - Ссылки и основные функции\n ' \
+                 f'5 - Продвинутые функции\n ' \
+                 f'6 - ВПР, ГПР, индексы и практические задачи\n '
     bot.send_message(call.message.chat.id, answer)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -37,6 +59,7 @@ def send_welcome(message):
                           f'/help узнать как работает подписка Brainskills\n'
                           f'/setting получать бесплатные материалы по Python, ML/DL, AI\n'
                           f'/action регистрироваться на бесплатные мероприятия')
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
