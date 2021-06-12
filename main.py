43478051a381a5341f87c391c09300124aeb9d18
bolovtsov <bol.ser.vla@gmail.com>
2021-06-12 20:56:18 +0300

    Запуск

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
 @bot.message_handler(content_types=['text'])
 def any_messages(message):
-    keyboard_main = telebot.types.InlineKeyboardMarkup(row_width=4)
+    keyboard_main = telebot.types.InlineKeyboardMarkup(row_width=2)
     about_company_button = telebot.types.InlineKeyboardButton(text='О Brainskills', callback_data="about")
     courses_button = telebot.types.InlineKeyboardButton(text='курсы', callback_data="courses")
     free_button = telebot.types.InlineKeyboardButton(text='материалы', callback_data="free")
     registration_button = telebot.types.InlineKeyboardButton(text='мероприятия', callback_data="registration")
-    keyboard_main.add(about_company_button, courses_button, free_button, registration_button)
+    keyboard_main.add(about_company_button)
+    keyboard_main.add(courses_button, free_button)
+    keyboard_main.add(registration_button)
     bot.send_message(message.chat.id, text="Что интересно?", reply_markup=keyboard_main)
 
 
@@ -27,7 +29,6 @@ def callback_inline(call):
         keyboard_main.add(about_company_button, courses_button, free_button, registration_button)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="то интересно?",
                               reply_markup=keyboard_main)
-
     if call.data == "courses":
         keyboard_courses = telebot.types.InlineKeyboardMarkup()
         python_button = telebot.types.InlineKeyboardButton(text='Анализ данных на Python', callback_data="python")
