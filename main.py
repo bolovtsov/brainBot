96215590b3c4d1caaf018a3e90b3920d8185be4b
bolovtsov <bol.ser.vla@gmail.com>
2021-06-12 21:41:57 +0300

    Запуск

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import constants
 
 bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
-img_book = open('img/1.png', 'rb')
 
 @bot.message_handler(content_types=['text'])
 def any_messages(message):
@@ -16,8 +15,9 @@ def any_messages(message):
     keyboard_main.add(about_company_button)
     keyboard_main.add(courses_button, free_button)
     keyboard_main.add(registration_button)
+    img_book = open('img/1.png', 'rb')
     bot.send_photo(message.chat.id, img_book, reply_markup=keyboard_main)
-    #bot.send_message(message.chat.id, text="Что интересно?", reply_markup=keyboard_main)
+    bot.send_message(message.chat.id, text="Welcome!", reply_markup=keyboard_main)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -31,7 +31,6 @@ def callback_inline(call):
         keyboard_main.add(about_company_button)
         keyboard_main.add(courses_button, free_button)
         keyboard_main.add(registration_button)
-        bot.send_photo(chat_id=call.message.chat.id, img_book, reply_markup=keyboard_main)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="то интересно?",
                               reply_markup=keyboard_main)
     if call.data == "courses":
@@ -40,7 +39,11 @@ def callback_inline(call):
         sql_button = telebot.types.InlineKeyboardButton(text='SQL/Oracle', callback_data="sql")
         excel_button = telebot.types.InlineKeyboardButton(text='Базовый Excel', callback_data="excel")
         back_button = telebot.types.InlineKeyboardButton(text='Назад', callback_data="mainmenu")
-        keyboard_courses.add(python_button, sql_button, excel_button, back_button)
+        keyboard_courses.add(python_button)
+        keyboard_courses.add(sql_button, excel_button, back_button)
+        with open('img/2.png', 'rb') as notebook:
+            bot.edit_message_media(media=types.InputMedia(type='photo', media=notebook), chat_id=call.message.chat.id,
+                                   message_id=call.message.message_id, reply_markup=keyboard_courses)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Курсы",
                               reply_markup=keyboard_courses)
     if call.data == "free":
@@ -49,7 +52,11 @@ def callback_inline(call):
         case_button = telebot.types.InlineKeyboardButton(text='кейс', callback_data='free_case')
         ml_button = telebot.types.InlineKeyboardButton(text='материалы по ML', callback_data='free_ML')
         back_button = telebot.types.InlineKeyboardButton(text='Назад', callback_data="mainmenu")
-        keyboard_free.add(base_python_button, case_button, ml_button, back_button)
+        keyboard_free.add(base_python_button)
+        keyboard_free.add(case_button, ml_button, back_button)
+        with open('img/2.png', 'rb') as notebook:
+            bot.edit_message_media(media=types.InputMedia(type='photo', media=notebook), chat_id=call.message.chat.id,
+                                   message_id=call.message.message_id, reply_markup=keyboard_free)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Материалы",
                               reply_markup=keyboard_free)
     if call.data == "about":
