180316425dd0a53cc2e8128616665057b266250f
bolovtsov <bol.ser.vla@gmail.com>
2021-06-12 22:04:29 +0300

    Запуск

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def callback_inline(call):
         keyboard_courses.add(python_button)
         keyboard_courses.add(sql_button, excel_button, back_button)
         with open('img/2.png', 'rb') as notebook:
-            bot.edit_message_media(media=types.InputMedia(type='photo', media=notebook),
+            bot.edit_message_media(media=telebot.types.InputMedia(type='photo', media=notebook),
                                    chat_id=call.message.chat.id,
                                    message_id=call.message.message_id,
                                    reply_markup=keyboard_courses)
