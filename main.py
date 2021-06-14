51e265848869c8544a52c4587d597b22b8af8907
bolovtsov <bol.ser.vla@gmail.com>
2021-06-14 12:00:20 +0300

    Запуск

--- main.py
+++ main.py
@@ -41,10 +41,13 @@ def callback_inline(call):
         back_button = telebot.types.InlineKeyboardButton(text='Назад', callback_data="mainmenu")
         keyboard_courses.add(python_button)
         keyboard_courses.add(sql_button, excel_button, back_button)
-        bot.edit_message_media(chat_id=call.message.chat.id,
+        with open('img/2.png', 'rb') as notebook:
+            bot.edit_message_media(chat_id=call.message.chat.id,
                                message_id=call.message.message_id,
-                               media=telebot.types.InputMediaPhoto("https://drive.google.com/file/d/10K-lTslaUNkn0c48hSPmzf5HVC3_koBw/view?usp=sharing"))
-        #bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id, media=telebot.InputMedia(type='photo', media=notebook))
+                               media=telebot.types.InputMediaPhoto(notebook))
+        #bot.edit_message_media(chat_id=call.message.chat.id,
+                              # message_id=call.message.message_id,
+                              # media=telebot.types.InputMediaPhoto("https://drive.google.com/file/d/10K-lTslaUNkn0c48hSPmzf5HVC3_koBw/view?usp=sharing"))
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Курсы",
                               reply_markup=keyboard_courses)
     if call.data == "free":
